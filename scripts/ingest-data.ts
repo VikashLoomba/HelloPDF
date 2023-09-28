fc71c121712eba342d7547097b244befd0db03c7
VikashLoomba <loomba.vikash@gmail.com>
2023-09-28 01:07:06 -0700

    Update ingest-data - split out `run` in to its own functions to eventually support web uploads

--- scripts/ingest-data.ts
+++ scripts/ingest-data.ts
@@ -4,11 +4,43 @@ import { DirectoryLoader } from 'langchain/document_loaders/fs/directory';
 import { PDFLoader } from 'langchain/document_loaders/fs/pdf'
 import { Chroma } from 'langchain/vectorstores/chroma';
 import { COLLECTION_NAME } from '@/config/chroma';
+import type { Document } from 'langchain/document';
 
 /* Name of directory to retrieve your files from */
 const filePath = 'docs';
 
 export const run = async () => {
+  try {
+    /*load raw docs from the all files in the directory */
+    const docs = await loadDocumentsFromPDF();
+
+    console.log('creating vector store...');
+    await generateEmbeddings(docs);
+  } catch (error) {
+    console.log('error', error);
+    throw new Error('Failed to ingest your data');
+  }
+};
+
+
+const generateEmbeddings = async (docs: Document<Record<string, any>>[]) => {
+  try {
+    /*create and store the embeddings in the vectorStore*/
+    const embeddings = new OpenAIEmbeddings();
+
+    let chroma = new Chroma(embeddings, { collectionName: COLLECTION_NAME });
+    await chroma.index?.reset();
+
+    await Chroma.fromDocuments(docs, embeddings, {
+      collectionName: COLLECTION_NAME,
+    });
+  } catch (error) {
+    console.log('error', error);
+    throw new Error('Failed to ingest your data');
+  }
+}
+
+const loadDocumentsFromPDF = async () => {
   try {
     /*load raw docs from the all files in the directory */
     const directoryLoader = new DirectoryLoader(filePath, {
@@ -26,22 +58,12 @@ export const run = async () => {
 
     const docs = await textSplitter.splitDocuments(rawDocs);
     console.log('split docs', docs);
-
-    console.log('creating vector store...');
-    /*create and store the embeddings in the vectorStore*/
-    const embeddings = new OpenAIEmbeddings();
-
-    let chroma = new Chroma(embeddings, { collectionName: COLLECTION_NAME });
-    await chroma.index?.reset();
-
-    await Chroma.fromDocuments(docs, embeddings, {
-      collectionName: COLLECTION_NAME,
-    });
+    return docs;
   } catch (error) {
     console.log('error', error);
     throw new Error('Failed to ingest your data');
   }
-};
+}
 
 (async () => {
   await run();
