2e31ef170c5bc26b6154c887d18b9a8a6c6895f1
VikashLoomba <loomba.vikash@gmail.com>
2023-10-05 11:41:25 -0700

    Update gitignore, remove unused references

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,6 @@
 import { useRef, useState, useEffect } from 'react';
 import Layout from '@/components/layout';
 import styles from '@/styles/Home.module.css';
-import { Message } from '@/types/chat';
 import Image from 'next/image';
 import ReactMarkdown from 'react-markdown';
 import LoadingDots from '@/components/ui/LoadingDots';
